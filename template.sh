c6a311f4d0c18c00a85d8e3721bd0c5c6134c938
Mark Sellors <github@5vcc.com>
2017-03-20 15:37:25 +0000

    Added initial draft of the template

---
+++ template.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+# taken from here - https://dev.to/thiht/shell-scripts-matter
+
+# ----
+
+# use shellcheck - https://www.shellcheck.net/
+
+# use shUnit2 - https://github.com/kward/shunit2
+
+# use bash debugger - http://bashdb.sourceforge.net/bashdb.html
+
+# ----
+
+# defaults for variables
+my_arg=${1:-"default"}
+
+# ----
+
+# do some cleanup
+cleanup() {
+    # ...
+}
+trap cleanup EXIT
+
+# ----
+
+# do some logging
+readonly LOG_FILE="/tmp/$(basename "$0").log"
+info()    { echo "$(date +%FT%T%z) [INFO]    $@" | tee -a "$LOG_FILE" >&2 ; }
+warn()    { echo "$(date +%FT%T%z) [WARN]    $@" | tee -a "$LOG_FILE" >&2 ; }
+error()   { echo "$(date +%FT%T%z) [ERROR]   $@" | tee -a "$LOG_FILE" >&2 ; }
+fatal()   { echo "$(date +%FT%T%z) [FATAL]   $@" | tee -a "$LOG_FILE" >&2 ; exit 1 ; }
+
+# ----
+
+# document your scripts
+#/ Usage: add <first number> <second number>
+#/ Compute the sum of two numbers
+usage() {
+    grep '^#/' "$0" | cut -c4-
+    exit 0
+}
+expr "$*" : ".*--help" > /dev/null && usage
+
+# uses lines starting #/ as the documentation
+# ----
+
+
+# check if script is sourced or being run directly
+if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
+    trap cleanup EXIT
+    # Script goes here
+    # ...
+fi
